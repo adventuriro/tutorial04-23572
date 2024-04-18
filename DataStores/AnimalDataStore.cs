c3b0dcc65aac34a82a101b8d3dd12328dd247c92
adventuriro <53796601+adventuriro@users.noreply.github.com>
2024-04-18 11:53:56 +0200

    Initial commit

---
+++ DataStores/AnimalDataStore.cs
@@ -0,0 +1,41 @@
+﻿using tutorial05.Models;
+
+namespace tutorial05.DataStores;
+
+public class AnimalsDataStore
+{
+    public List<Animal> Animals { get; set; }
+
+    public static AnimalsDataStore Current { get; } = new AnimalsDataStore();
+
+    public AnimalsDataStore()
+    {
+        Animals = new List<Animal>()
+        {
+            new Animal
+            {
+                Id = 1,
+                Name = "Fifek",
+                Category = "Dog",
+                Color = "White",
+                Weight = 12,
+            },
+            new Animal
+            {
+                Id = 2,
+                Name = "Miauczek",
+                Category = "Cat",
+                Color = "Orange",
+                Weight = 7,
+            },
+            new Animal
+            {
+                Id = 3,
+                Name = "Owca",
+                Category = "Sheep",
+                Color = "Black",
+                Weight = 9,
+            },
+        };
+    }
+}
---
+++ Models/Animal.cs
@@ -0,0 +1,10 @@
+﻿namespace tutorial05.Models;
+
+public class Animal
+{
+    public required int Id { get; init; }
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public double Weight { get; set; }
+    public string Color { get; set; }
+}
